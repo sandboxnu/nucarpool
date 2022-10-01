ca0299d98d71c8c2ab7701b29815b6b11625ce9e
Dillon Scott <dillonscott.2024@gmail.com>
2022-10-01 19:04:46 -0400

    Router documentation goes brr

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,8 +3,8 @@ import { createRouter } from "./createRouter";
 import { mapboxRouter } from "./mapbox";
 import { userRouter } from "./user";
 
-// TODO: Document this
-
+// This bundles together our distinct routers into one router, with prefixes for respective routers
+// The superjson transformer does JSON type conversion for Dates, Maps, and Sets for us :D
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge("user.", userRouter)
