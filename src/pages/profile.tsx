487fec57d745846869007c511184c50679533068
owensimpson <owensimpson1127@gmail.com>
2023-03-03 19:49:51 -0500

    accidental debounce constant changes

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -137,13 +137,13 @@ const Profile: NextPage = () => {
 
   const [companyAddress, setCompanyAddress] = useState("");
   const updateCompanyAddress = useMemo(
-    () => debounce(setCompanyAddress, 500),
+    () => debounce(setCompanyAddress, 1000),
     []
   );
 
   const [startingAddress, setStartingAddress] = useState("");
   const updateStartingAddress = useMemo(
-    () => debounce(setStartingAddress, 500),
+    () => debounce(setStartingAddress, 1000),
     []
   );
 
