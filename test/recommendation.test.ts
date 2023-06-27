31826fa9c5f2a60f071ebd2718bff1cdb41c3857
owensimpson <owensimpson1127@gmail.com>
2023-06-27 10:18:51 -0400

    fixed recs

--- test/recommendation.test.ts
+++ test/recommendation.test.ts
@@ -2,16 +2,16 @@ import calculateScore, { generateUser } from "../src/utils/recommendation";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
-import { expect, jest, test } from "@jest/globals";
-import { Prisma, User } from "@prisma/client";
+import { expect, test } from "@jest/globals";
+import { Role, User } from "@prisma/client";
 import _ from "lodash";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-const relativeOrderBaseUser: User = generateUser({
+const relativeOrderBaseDriver: User = generateUser({
   id: "0",
-  role: "DRIVER",
+  role: Role.DRIVER,
   seatAvail: 1,
   companyCoordLng: 42.35,
   companyCoordLat: -71.06,
@@ -26,176 +26,190 @@ const relativeOrderBaseUser: User = generateUser({
   endTime: "17:00",
 }).create;
 
-const calcScoreForBaseUser = calculateScore(relativeOrderBaseUser);
+const relativeOrderBaseRider = {
+  ...relativeOrderBaseDriver,
+  role: Role.RIDER,
+};
+
+const calcScoreForBaseDriver = calculateScore(relativeOrderBaseDriver);
+const calcScoreForBaseRider = calculateScore(relativeOrderBaseRider);
 
 const usersToBeCutoff: User[] = [
   {
-    ...relativeOrderBaseUser,
+    ...relativeOrderBaseRider,
     daysWorking: "1,1,0,0,0,0,1",
   },
   {
-    ...relativeOrderBaseUser,
+    ...relativeOrderBaseRider,
     startTime: dayjs.tz("2022-11-01 07:59:00", "UTC").toDate(),
   },
   {
-    ...relativeOrderBaseUser,
+    ...relativeOrderBaseRider,
     endTime: dayjs.tz("2022-11-01 18:01:00", "UTC").toDate(),
   },
   {
-    ...relativeOrderBaseUser,
-    startCoordLat: relativeOrderBaseUser.startCoordLat + 0.05,
+    ...relativeOrderBaseRider,
+    startCoordLat: relativeOrderBaseDriver.startCoordLat + 0.05,
   },
   {
-    ...relativeOrderBaseUser,
-    startCoordLng: relativeOrderBaseUser.startCoordLng + 0.05,
+    ...relativeOrderBaseRider,
+    startCoordLng: relativeOrderBaseDriver.startCoordLng + 0.05,
   },
   {
-    ...relativeOrderBaseUser,
-    companyCoordLat: relativeOrderBaseUser.companyCoordLat + 0.05,
+    ...relativeOrderBaseRider,
+    companyCoordLat: relativeOrderBaseDriver.companyCoordLat + 0.05,
   },
   {
-    ...relativeOrderBaseUser,
-    companyCoordLng: relativeOrderBaseUser.companyCoordLng + 0.05,
+    ...relativeOrderBaseRider,
+    companyCoordLng: relativeOrderBaseRider.companyCoordLng + 0.05,
   },
 ];
 
 const usersToNotBeCutoff: User[] = [
   {
-    ...relativeOrderBaseUser,
+    ...relativeOrderBaseRider,
     daysWorking: "1,1,0,0,0,1,1",
   },
   {
-    ...relativeOrderBaseUser,
+    ...relativeOrderBaseRider,
     startTime: dayjs.tz("2022-11-01 08:00:00", "UTC").toDate(),
   },
   {
-    ...relativeOrderBaseUser,
+    ...relativeOrderBaseRider,
     endTime: dayjs.tz("2022-11-01 18:00:00", "UTC").toDate(),
   },
   {
-    ...relativeOrderBaseUser,
-    startCoordLat: relativeOrderBaseUser.startCoordLat + 0.04,
+    ...relativeOrderBaseRider,
+    startCoordLat: relativeOrderBaseDriver.startCoordLat + 0.04,
   },
   {
-    ...relativeOrderBaseUser,
-    startCoordLng: relativeOrderBaseUser.startCoordLng + 0.04,
+    ...relativeOrderBaseRider,
+    startCoordLng: relativeOrderBaseDriver.startCoordLng + 0.04,
   },
   {
-    ...relativeOrderBaseUser,
-    companyCoordLat: relativeOrderBaseUser.companyCoordLat + 0.04,
+    ...relativeOrderBaseRider,
+    companyCoordLat: relativeOrderBaseDriver.companyCoordLat + 0.04,
   },
   {
-    ...relativeOrderBaseUser,
-    companyCoordLng: relativeOrderBaseUser.companyCoordLng + 0.04,
+    ...relativeOrderBaseRider,
+    companyCoordLng: relativeOrderBaseDriver.companyCoordLng + 0.04,
   },
 ];
 
 test("Test that users outside of cutoffs are not included", () => {
-  const recs = _.compact(usersToBeCutoff.map(calcScoreForBaseUser));
+  const recs = _.compact(usersToBeCutoff.map(calcScoreForBaseDriver));
   expect(recs.length).toEqual(0);
 });
 
 test("Test that users inside of cutoffs are included", () => {
-  const recs = _.compact(usersToNotBeCutoff.map(calcScoreForBaseUser));
+  const recs = _.compact(usersToNotBeCutoff.map(calcScoreForBaseDriver));
   expect(recs.length).toEqual(usersToNotBeCutoff.length);
 });
 
 const relativeOrderUsers: User[] = [
   {
-    ...relativeOrderBaseUser,
-    role: "RIDER",
+    ...relativeOrderBaseRider,
   },
   {
-    ...relativeOrderBaseUser,
-    role: "DRIVER",
-  },
-  {
-    ...relativeOrderBaseUser,
-    role: "RIDER",
+    ...relativeOrderBaseRider,
     daysWorking: "0,1,0,1,1,1,0",
   },
   {
-    ...relativeOrderBaseUser,
-    role: "RIDER",
+    ...relativeOrderBaseRider,
     startTime: dayjs.tz("2022-11-01 09:15:00", "UTC").toDate(),
   },
   {
-    ...relativeOrderBaseUser,
-    role: "RIDER",
+    ...relativeOrderBaseRider,
     endTime: dayjs.tz("2022-11-01 17:15:00", "UTC").toDate(),
   },
   {
-    ...relativeOrderBaseUser,
-    role: "RIDER",
-    startCoordLat: relativeOrderBaseUser.startCoordLat + 0.001,
-    startCoordLng: relativeOrderBaseUser.startCoordLng + 0.001,
+    ...relativeOrderBaseRider,
+    startCoordLat: relativeOrderBaseDriver.startCoordLat + 0.001,
+    startCoordLng: relativeOrderBaseDriver.startCoordLng + 0.001,
   },
   {
-    ...relativeOrderBaseUser,
-    role: "RIDER",
-    companyCoordLat: relativeOrderBaseUser.companyCoordLat + 0.001,
-    companyCoordLng: relativeOrderBaseUser.companyCoordLng + 0.001,
+    ...relativeOrderBaseRider,
+    companyCoordLat: relativeOrderBaseDriver.companyCoordLat + 0.001,
+    companyCoordLng: relativeOrderBaseDriver.companyCoordLng + 0.001,
   },
   {
-    ...relativeOrderBaseUser,
-    role: "RIDER",
+    ...relativeOrderBaseRider,
     daysWorking: "0,1,0,1,1,1,1",
   },
   {
-    ...relativeOrderBaseUser,
+    ...relativeOrderBaseRider,
+    daysWorking: "0,1,0,1,1,1,1",
     startTime: null,
+  },
+  {
+    ...relativeOrderBaseDriver,
+  },
+  {
+    ...relativeOrderBaseDriver,
+    startTime: null,
+    daysWorking: "0,1,0,1,1,1,0",
+  },
+  {
+    ...relativeOrderBaseDriver,
+    endTime: null,
     daysWorking: "0,1,0,1,1,1,0",
   },
 ];
 
-const relativeScores = _.compact(
-  relativeOrderUsers.map(calcScoreForBaseUser)
+const relativeDriverScores = _.compact(
+  relativeOrderUsers.map(calcScoreForBaseDriver)
+).map((r) => r.score);
+
+const relativeRiderScores = _.compact(
+  relativeOrderUsers.map(calcScoreForBaseRider)
 ).map((r) => r.score);
 
 test("all relative order users within cutoffs", () => {
-  expect(relativeScores).toHaveLength(relativeOrderUsers.length);
+  expect(relativeDriverScores).toHaveLength(
+    relativeOrderUsers.filter((user) => user.role == Role.RIDER).length
+  );
 });
 
 test("perfect match", () => {
-  expect(relativeScores[0]).toEqual(0);
+  expect(relativeDriverScores[0]).toEqual(0);
 });
 
 test("driver deprioritization", () => {
-  expect(relativeScores[0]).toBeLessThan(relativeScores[1]);
+  expect(relativeDriverScores[0]).toBeLessThan(relativeDriverScores[1]);
 });
 
 test("less shared daysWorking raises score", () => {
-  expect(relativeScores[0]).toBeLessThan(relativeScores[2]);
+  expect(relativeDriverScores[0]).toBeLessThan(relativeDriverScores[2]);
 });
 
 test("extra daysWorking doesn't affect score", () => {
-  expect(relativeScores[2]).toEqual(relativeScores[7]);
+  expect(relativeDriverScores[1]).toEqual(relativeDriverScores[6]);
 });
 
 test("differing startTime raises score", () => {
-  expect(relativeScores[0]).toBeLessThan(relativeScores[3]);
+  expect(relativeDriverScores[0]).toBeLessThan(relativeDriverScores[3]);
 });
 
 test("differing endTime raises score", () => {
-  expect(relativeScores[0]).toBeLessThan(relativeScores[4]);
+  expect(relativeDriverScores[0]).toBeLessThan(relativeDriverScores[4]);
 });
 
 test("start and endTime have same effect on score", () => {
-  expect(relativeScores[3]).toEqual(relativeScores[4]);
+  expect(relativeRiderScores[1]).toEqual(relativeRiderScores[2]);
 });
 
 test("differing start location raises score", () => {
-  expect(relativeScores[0]).toBeLessThan(relativeScores[5]);
+  expect(relativeDriverScores[0]).toBeLessThan(relativeDriverScores[5]);
 });
 
 test("differing company location raises score", () => {
-  expect(relativeScores[0]).toBeLessThan(relativeScores[6]);
+  expect(relativeDriverScores[0]).toBeLessThan(relativeDriverScores[6]);
 });
 
 test("Company location difference has larger effect on score", () => {
-  expect(relativeScores[5]).toBeLessThan(relativeScores[6]);
+  expect(relativeDriverScores[5]).toBeLessThan(relativeDriverScores[6]);
 });
 
 test("Missing start time is worse than same time when other details differ", () => {
-  expect(relativeScores[2]).toBeLessThan(relativeScores[8]);
+  expect(relativeDriverScores[1]).toBeLessThan(relativeDriverScores[7]);
 });
