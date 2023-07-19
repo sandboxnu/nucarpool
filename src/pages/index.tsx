2842d68e82bff11f22c83000061d089b560d23b0
Devashish Sood <39184335+devashish9@users.noreply.github.com>
2023-07-19 23:52:34 +0530

    Received Modal Bug Fix (#64)
    * fixes for received modal not deleting requests
    
    * added todo for future

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -128,7 +128,7 @@ const Home: NextPage<any> = () => {
   };
 
   const handleRejectRequest = (fromUser: PublicUser) => {
-    const userRequest = sent.find(
+    const userRequest = received.find(
       (request) => request.fromUser?.name === fromUser.name
     );
     if (userRequest) {
@@ -136,7 +136,16 @@ const Home: NextPage<any> = () => {
     }
   };
 
-  const handleAcceptRequest = (fromUser: PublicUser) => {};
+  const handleAcceptRequest = (fromUser: PublicUser) => {
+    // Must also handle group inclusion functionality here
+    // When the carpooling page is complete
+    const userRequest = received.find(
+      (request) => request.fromUser?.name === fromUser.name
+    );
+    if (userRequest) {
+      handleDeleteRequest(userRequest);
+    }
+  };
 
   const handleDeleteRequest = (request: ResolvedRequest) => {
     deleteRequest({
