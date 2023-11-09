178b6c4268d17c1a6a55c3c3a487dd1b96d9be5d
Devashish Sood <39184335+devashish9@users.noreply.github.com>
2023-11-09 15:17:03 -0500

    Email Templates (#78)
    * connect email setup
    
    * cleanup
    
    * remove debug email
    
    * final legal language:

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,40 +1,27 @@
 import { SendEmailCommandInput } from "@aws-sdk/client-ses";
 
-export const generateParams = (schema: emailSchema): SendEmailCommandInput => {
-  //TODO: When the AWS account is moved out of the sandbox env, we remove this check
-  const dest: string =
-    process.env.NODE_ENV === "production"
-      ? schema.receivingUser
-      : "carpoolnu@gmail.com";
-  const cc: string =
-    process.env.NODE_ENV === "production"
-      ? schema.sendingUser
-      : "devashishsood9@gmail.com";
-  return {
-    Destination: {
-      CcAddresses: [cc],
-      ToAddresses: [dest],
-    },
-    Message: {
-      Body: {
-        Text: {
-          Charset: "UTF-8",
-          Data: schema.body,
-        },
-      },
-      Subject: {
-        Charset: "UTF-8",
-        Data: schema.subject,
-      },
-    },
+export const generateConnectEmailparams = (schema: emailSchema) => {
+  console.log(schema.sendingUserEmail);
+  const params = {
     Source: "no-reply@carpoolnu.com",
-    ReplyToAddresses: [],
+    Destination: {
+      ToAddresses: [schema.receivingUserEmail],
+      CcAddresses: [schema.sendingUserEmail],
+    },
+    Template: "UserReceivedRequest",
+    TemplateData: JSON.stringify({
+      preferredName: schema.sendingUserName,
+      OtherUser: schema.receivingUserName,
+      message: schema.body,
+    }),
   };
+  return params;
 };
 
 export interface emailSchema {
-  sendingUser: string;
-  receivingUser: string;
-  subject: string;
+  sendingUserName: string;
+  sendingUserEmail: string;
+  receivingUserName: string;
+  receivingUserEmail: string;
   body: string;
 }
