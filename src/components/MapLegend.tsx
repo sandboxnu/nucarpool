88166965ad4a975579aff0c500668bb281221a17
Graham Mather <mather.g@northeastern.edu>
2024-07-23 13:12:03 -0400

    GeoJsonData now will return riders and drivers in viewer mode, drivers and riders are color coded

--- src/components/MapLegend.tsx
+++ src/components/MapLegend.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 interface LegendProps {
   role: string;
 }
+// Currently this isn't used anywhere
 export const MapLegend = (props: LegendProps) => {
   return (
     <>
--- src/utils/map/addClusters.ts
+++ src/utils/map/addClusters.ts
@@ -1,6 +1,7 @@
 import { Map } from "mapbox-gl";
 import { GeoJsonUsers } from "../types";
-import OrangeCircle from "../../../public/orange-square.png";
+import OrangeSquare from "../../../public/orange-square.png";
+import RedSquare from "../../../public/red-square.png";
 /**
  * Filter Expression: https://docs.mapbox.com/mapbox-gl-js/style-spec/expressions/
  * Clusters example with filter expression: https://docs.mapbox.com/mapbox-gl-js/example/cluster-html/
@@ -56,22 +57,44 @@ const addClusters = (map: Map, geoJsonUsers: GeoJsonUsers) => {
       "text-size": 12,
     },
   });
+  console.log("Adding source to map with GeoJSON data:", geoJsonUsers); // Log GeoJSON data
 
-  map.loadImage(OrangeCircle.src, (error, image) => {
-    if (error || !image) throw error;
+  // Add Driver Locations
+  map.loadImage(OrangeSquare.src, (error, orangeImage) => {
+    if (error || !orangeImage) throw error;
+    map.addImage("rider-marker", orangeImage);
+    map.loadImage(RedSquare.src, (error, blueImage) => {
+      if (error || !blueImage) throw error;
+      map.addImage("driver-marker", blueImage);
+      // Add the image to the map style.
+      // map.addImage("end_locs", image);
 
-    // Add the image to the map style.
-    map.addImage("end_locs", image);
-
-    map.addLayer({
-      id: "unclustered-point",
-      type: "symbol",
-      source: "company-locations",
-      filter: ["!", ["has", "point_count"]],
-      layout: {
-        "icon-image": "end_locs",
-        "icon-size": 0.35,
-      },
+      map.addLayer({
+        id: "unclustered-point",
+        type: "symbol",
+        source: "company-locations",
+        filter: ["!", ["has", "point_count"]],
+        layout: {
+          "icon-image": [
+            "match",
+            ["get", "role"],
+            "RIDER",
+            "rider-marker",
+            "DRIVER",
+            "driver-marker",
+            "rider-marker",
+          ],
+          "icon-size": [
+            "match",
+            ["get", "role"], // Get the role from each feature's properties
+            "DRIVER",
+            1,
+            "RIDER",
+            0.35,
+            0.35,
+          ],
+        },
+      });
     });
   });
 };
