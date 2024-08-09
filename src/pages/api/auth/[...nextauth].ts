cae82ead4c57d315d79ca6eafba6ffdd97d93eb1
Graham Mather <mather.g@northeastern.edu>
2024-08-09 11:29:54 -0400

    Env for google login logic

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,6 +6,7 @@ import { serverEnv } from "../../../utils/env/server";
 import AzureADProvider from "next-auth/providers/azure-ad";
 import GoogleProvider from "next-auth/providers/google";
 import { Adapter } from "next-auth/adapters";
+import { browserEnv } from "../../../utils/env/browser";
 
 const CustomPrismaAdapter = (p: typeof prisma): Adapter => {
   return {
@@ -49,10 +50,14 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: CustomPrismaAdapter(prisma),
   providers: [
-    GoogleProvider({
-      clientId: serverEnv.GOOGLE_CLIENT_ID,
-      clientSecret: serverEnv.GOOGLE_CLIENT_SECRET,
-    }),
+    ...(browserEnv.NEXT_PUBLIC_ENV === "staging"
+      ? [
+          GoogleProvider({
+            clientId: serverEnv.GOOGLE_CLIENT_ID,
+            clientSecret: serverEnv.GOOGLE_CLIENT_SECRET,
+          }),
+        ]
+      : []),
     AzureADProvider({
       clientId: serverEnv.AZURE_CLIENT_ID,
       clientSecret: serverEnv.AZURE_CLIENT_SECRET,
