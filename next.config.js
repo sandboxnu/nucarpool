086842e8e4c3c5fd746a21184e025543636be318
aaryansinha <sinha.aar@northeastern.edu>
2024-08-30 13:53:36 -0400

    Next config fixes for view route

--- next.config.js
+++ next.config.js
@@ -7,14 +7,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-
-    return config;
-  }
 };
 
 module.exports = nextConfig;
