ed83e87585a6c47019de4376a0ae830ba424588d
Graham Mather <mather.g@northeastern.edu>
2024-09-18 11:03:14 -0400

    Header placement adjustment

--- src/components/DropDownMenu.tsx
+++ src/components/DropDownMenu.tsx
@@ -48,7 +48,7 @@ const DropDownMenu = () => {
         </div>
       )}
       <Menu>
-        <Menu.Button className="h-12 w-12 overflow-hidden rounded-full">
+        <Menu.Button className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-full">
           {profileImageUrl && !imageLoadError ? (
             <Image
               src={profileImageUrl}
