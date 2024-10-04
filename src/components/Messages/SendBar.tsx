a1cf36f88770dee50d4dc5c539f0eef31c738b99
Graham Mather <mather.g@northeastern.edu>
2024-10-04 13:01:32 -0400

    Sendbar ui update

--- src/components/Messages/SendBar.tsx
+++ src/components/Messages/SendBar.tsx
@@ -16,7 +16,7 @@ const SendBar = ({ onSendMessage }: SendBarProps) => {
     }
   };
 
-  const handleKeyPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
     if (e.key === "Enter" && !e.shiftKey) {
       e.preventDefault();
       handleSend();
@@ -26,16 +26,28 @@ const SendBar = ({ onSendMessage }: SendBarProps) => {
   return (
     <div className="border-t p-6">
       <div className="mx-10 flex items-center overflow-hidden rounded-lg border bg-gray-100">
-        <textarea
-          className="flex-1 resize-none border-0 bg-gray-100 p-2 text-lg focus:outline-none"
-          rows={1}
+        <div
+          contentEditable="true"
+          className="w-full flex-1 resize-none border-0 bg-gray-100 p-2 text-lg focus:outline-none"
           placeholder="Type a message..."
-          value={messageContent}
-          onChange={(e) => setMessageContent(e.target.value)}
+          style={{
+            minHeight: "20px",
+            maxHeight: "100px",
+            lineHeight: "normal",
+            display: "inline-block",
+            whiteSpace: "pre-wrap",
+            overflowY: "auto",
+            overflowWrap: "break-word",
+          }}
+          onInput={(e) =>
+            setMessageContent(
+              e.currentTarget.textContent!! ? e.currentTarget.textContent : ""
+            )
+          }
           onKeyDown={handleKeyPress}
-        />
-        <div className="h-10 w-px  bg-gray-300" />
-        <button onClick={handleSend} className="p-2 px-4 pt-3">
+        ></div>
+        <div className="h-10 w-px bg-gray-300" />
+        <button onClick={handleSend} className=" p-2 px-4 pt-3">
           <Image src={sendIcon} alt="Send" width={26} height={26} />
         </button>
       </div>
