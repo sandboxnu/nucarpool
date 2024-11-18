f8a64959e94ad7063288519c9d054414344c02df
Graham Mather <mather.g@northeastern.edu>
2024-11-18 13:57:43 -0500

    Custom pfp same size as default

--- src/components/UserCards/UserCard.tsx
+++ src/components/UserCards/UserCard.tsx
@@ -108,9 +108,9 @@ export const UserCard = (props: UserCardProps): JSX.Element => {
           <Image
             src={profileImageUrl}
             alt={`${props.otherUser.preferredName}'s Profile Image`}
-            width={48}
-            height={48}
-            className="h-12 w-12  rounded-full object-cover"
+            width={56}
+            height={56}
+            className="h-14 w-14  rounded-full object-cover"
           />
         ) : (
           <AiOutlineUser className="h-14 w-14  rounded-full bg-gray-200" />
